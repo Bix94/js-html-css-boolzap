06459eec2f477b57a04e11ce7ba29192c83427de
Giacomo <giacomobiffi94@gmail.com>
2021-02-02 12:00:43 +0100

    chat whatsapp

---
+++ work/js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#prova',
+    data: {
+        contatto: [
+            {
+              user: 'Davide',
+              avatar:
+            },
+            {
+              user: 'Yuri',
+              avatar:
+            },
+            {
+              user: 'Emanuele',
+              avatar:
+            },
+            {
+              user: 'Niccolo',
+              avatar:
+            },
+            {
+              user: 'Cristiano',
+              avatar:
+            },
+            {
+              user: 'Giacomo',
+              avatar:
+            }
+        ],
+})
